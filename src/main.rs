f990ac1c9dcf7254da3d2fda23559c8cc52c0896
Xerydian <xerydian@gmail.com>
2024-10-23 11:21:45 +0200

    Refactor Pt. I

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,94 @@
 // Reference: https://github.com/sourcebox/mi-plaits-dsp-rs/blob/firmware-1.2/examples/midi control.rs
 // Reference: https://github.com/Narsil/rdev/blob/main/examples/listen.rs
 
-use std::thread;
 
-use std::sync::{Arc, Mutex};
+use std::{array, sync::{Arc, LazyLock, Mutex}, thread};
 
 mod audio_shell;
-use audio_shell::{AudioShell, AudioGenerator};
 
 mod synth;
-use synth::Synth;
-use rdev::Event;
+use synth::{SeqStatus, SeqStep, Synth};
+use crate::gui::process_events;
 
 mod keyboard_utils;
 
+
 const SAMPLE_RATE: u32 = 48000;
 const BLOCK_SIZE: usize = 2048;
+const MAX_VOICE_COUNT: usize = 4;
+
+use tinyaudio::{run_output_device, OutputDeviceParameters};
+
+mod gui;
+
+static TEMPO: f32 = 120.;
+static SEQ_STATUS: SeqStatus = SeqStatus::Stop;
+static SELECTED_VOICE: usize = 0;
+pub static VOICES: LazyLock<[Arc<Mutex<Synth<'static>>>; MAX_VOICE_COUNT]> = std::sync::LazyLock::new(||
+    array::from_fn(|_| Arc::new(Mutex::new(Synth::new(BLOCK_SIZE))))
+);
 
 fn main() {
-    let synth = Arc::new(Mutex::new(Synth::new(BLOCK_SIZE)));
-    let _audio_out = AudioShell::spawn(SAMPLE_RATE, BLOCK_SIZE, synth.clone());
-
-    let synth_1 = synth.clone();
-    let _sequencer = thread::spawn(move || {
-        Synth::sequencer_loop(synth_1);
-    });
-
-    let synth_2 = synth.clone();
-    let _smoothing = thread::spawn(move || {
-        Synth::control_loop(synth_2);
-    });
     
-    let synth_3 = synth.clone();
-    let _keyboard = rdev::listen(move |event: Event| {
-        synth_3.lock().unwrap().process_events(event);
-    }); // handle keystrokes, blocking
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: SAMPLE_RATE as usize,
+        channel_sample_count: BLOCK_SIZE,
+    };
+    
+    let _output_device = run_output_device(params, move |data| {
+        let mut samples_left = vec![0.0; BLOCK_SIZE];
+        let mut samples_right = vec![0.0; BLOCK_SIZE];
+        output_sound(&mut samples_left, &mut samples_right);
+
+        for (frame_no, samples) in data.chunks_mut(params.channels_count).enumerate() {
+            samples[0] = samples_left[frame_no];
+            samples[1] = samples_right[frame_no];
+        }
+    }).unwrap();
+
+    for synth in VOICES.iter() {       
+        synth.lock().unwrap().init(); 
+        let synth_1 = synth.clone();
+        thread::spawn(move || {
+            Synth::sequencer_loop(synth_1);
+        });
+
+        let synth_2 = synth.clone();
+        thread::spawn(move || {
+            Synth::control_loop(synth_2);
+        });
+    }
+
+    let _ = rdev::listen(process_events); // handle keystrokes, blocking
+}
+
+
+
+fn output_sound(samples_l: &mut [f32], samples_r: &mut [f32]) {
+    let mut out = [[0.0; BLOCK_SIZE]; MAX_VOICE_COUNT];
+    let mut aux = [[0.0; BLOCK_SIZE]; MAX_VOICE_COUNT];
+
+    for (i, synth) in VOICES.iter().enumerate() {
+        let out_i = out.get_mut(i).unwrap();
+        let aux_i = aux.get_mut(i).unwrap();
+        synth.lock().unwrap().render(out_i, aux_i);
+        synth.clear_poison();
+    }
+
+    for (i, synth) in VOICES.iter().enumerate() {
+        let v = synth.lock().unwrap();
+        let out_i = out.get(i).unwrap().to_owned();
+        let aux_i = aux.get(i).unwrap().to_owned();
+        let (pan_r, pan_l) = equal_power_panlaw_r_l(v.pan);
+        for frame in 0..BLOCK_SIZE {
+            let sample = (out_i[frame] * (1.0 - v.balance) + aux_i[frame] * v.balance) * v.volume;
+            samples_l[frame] += sample * pan_l;
+            samples_r[frame] += sample * pan_r;
+        }
+    }
+}
+
+fn equal_power_panlaw_r_l (pan: f32) -> (f32, f32) {
+    (pan * std::f32::consts::FRAC_PI_2).sin_cos() // right, left
 }
--- src/synth.rs
+++ src/synth.rs
@@ -1,7 +1,7 @@
 use crate::audio_shell::AudioGenerator;
 use mi_plaits_dsp::dsp::voice::{Modulations, Patch, Voice};
 
-use crate::keyboard_utils::ch;
+use crate::keyboard_utils::{ch, VK_Comma, VK_LeftBracket, VK_RightBracket, VK_B, VK_C, VK_D, VK_E, VK_F, VK_G, VK_H, VK_I, VK_J, VK_K, VK_M, VK_N, VK_O, VK_R, VK_S, VK_T, VK_U, VK_V, VK_W, VK_X, VK_Y, VK_Z};
 
 use std::vec;
 use std::collections::HashMap;
@@ -25,14 +25,14 @@ pub enum SeqStatus {
 
 #[derive(Default, Clone, Copy, Debug)]
 pub struct SeqStep {
-    note: Option<f32>,
-    model: Option<usize>,
-    harmonic: Option<f32>,
-    timbre: Option<f32>,
-    morph: Option<f32>,
-    decay: Option<f32>,
-    gate_length: Option<f64>,
-    is_awaiting_note: bool
+    pub note: Option<f32>,
+    pub model: Option<usize>,
+    pub harmonic: Option<f32>,
+    pub timbre: Option<f32>,
+    pub morph: Option<f32>,
+    pub decay: Option<f32>,
+    pub gate_length: Option<f64>,
+    pub is_awaiting_note: bool
 }
 
 pub enum Param  {
@@ -55,23 +55,29 @@ const PARAM_SMOOTH_FACTOR_INV: f32 = 1. - PARAM_SMOOTH_FACTOR;
 const VIBRATO_DEPTH: f32 = 0.6;
 const VIBRATO_RATE: f32 = std::f32::consts::PI * 10.;
 
+#[derive(Debug)]
 pub struct Synth<'a> {
 
     block_size: usize,
 
-    voice: Voice<'a>,
+    pub synth_engine: Voice<'a>,
     pub patch: Patch,
     pub modulations: Modulations,
 
-    // volume: f32,
-    // balance: f32,
+    pub volume: f32,
+    pub pan: f32,
+    pub balance: f32,
+
     note: f32,
     transpose: f32,
     rec_transpose: f32,
-    info_octave: i16,
+    pub info_octave: i16,
 
-    target_harmonic: f32, target_timbre: f32, target_morph:f32, target_bend: f32, target_vibrato_amount: f32,
-    smooth_harmonic: f32, smooth_timbre: f32, smooth_morph:f32, smooth_bend: f32, smooth_vibrato_amount: f32,
+    pub target_harmonic: f32, smooth_harmonic: f32,
+    pub target_timbre: f32, smooth_timbre: f32,
+    pub target_morph:f32, smooth_morph:f32,
+    target_vibrato_amount: f32, smooth_vibrato_amount: f32,
+    target_bend: f32, smooth_bend: f32, 
 
     pub tempo: f64,
     pub gate_length: f64,
@@ -86,12 +92,13 @@ impl<'a> Synth<'a> {
         Self {
             block_size,
 
-            voice: Voice::new(&std::alloc::System, block_size),
+            synth_engine: Voice::new(&std::alloc::System, block_size),
             patch: Patch::default(),
             modulations: Modulations::default(),
 
-            // volume: 1.0,
-            // balance: 0.0,
+            volume: 1.0,
+            balance: 0.0,
+            pan: 0.5,
             note: 48.0,
             transpose: 48.0,
             rec_transpose: 0.,
@@ -110,63 +117,8 @@ impl<'a> Synth<'a> {
         }
     }
 
-    fn print_info (&self) {
-        clearscreen::clear().unwrap();
-        println!("[ F1-F2 ]    Model: {}", ENGINE_DESCRIPIONS[self.patch.engine]);
-        println!("[ F3-F4 ] Harmonic: {}", (10. * self.target_harmonic).round() / 10.);
-        println!("[ F5-F6 ]   Timbre: {}", (10. * self.target_timbre).round() / 10.);
-        println!("[ F7-F8 ]    Morph: {}", (10. * self.target_morph).round() / 10.);
-        println!("[ F9-10 ]    Decay: {}", (10. * self.patch.decay).round() / 10.);
-        println!("");
-        println!("                           [ {} Rest ] [ {} {} ] [ BKSP Clear ]", 
-            ch(VK_LeftBracket), ch(VK_RightBracket),
-            if self.seq_status != SeqStatus::Recording {"Record"} else {"  Undo"},
-        );
-        println!("+-----------------------------------------------------------------+");
-        println!("|    {}   {}   {}   {}   {}   {}   {}       3   4   5   6   7   8   9    |", 
-            ch(VK_S), ch(VK_D), ch(VK_F), ch(VK_G), ch(VK_H), ch(VK_J), ch(VK_K),
-        );
-        println!("|  {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}   {}  |",
-            ch(VK_Z), ch(VK_X), ch(VK_C), ch(VK_V), ch(VK_B), ch(VK_N), ch(VK_M), ch(VK_Comma),
-            ch(VK_W), ch(VK_E), ch(VK_R), ch(VK_T), ch(VK_Y), ch(VK_U), ch(VK_I), ch(VK_O),
-        );
-        println!("+-----------------------------------------------------------------+");
-        println!("[ LSHIFT  > ] Pitch Bend    [ LCTRL ] Vibrato   [ . - ]  Octave: {} ", self.info_octave);
-        println!("                     [   SPACE_BAR    {:?}   ]",
-            if self.seq_status != SeqStatus::Play {"Play"} else {"Stop"}
-        );
-        self.print_sequence();
-        println!("                 Transport: {:?}", self.seq_status);
-        println!("[   Up / Down  ] Tempo: {} BPM", self.tempo.round());
-        println!("[ Left / Right ] Gate length: {}", (10. * self.gate_length).round() / 10.);
-        println!("");
-        println!("(Press [Esc] to exit)");
-        // println!("")
-        // println!("(debug) Rec Transpose: {}", self.rec_transpose);
-    }
-
-    pub fn print_sequence (&self) {
-        if self.seq_status == SeqStatus::Recording || !self.seq_notes.is_empty() {
-            println!("");
-            println!("Sequence: ");
-        }
-        if self.seq_status == SeqStatus::Recording && self.seq_notes.is_empty() {
-            println!("[ ]");
-        }
-        for (i, sq) in self.seq_notes.iter().enumerate() {
-            if i % 8 == 0 { print!("["); }
-            match sq {
-                SeqStep { is_awaiting_note: true, ..  } => print!("   MOD  "),
-                SeqStep { note: None, model: None, harmonic: None, timbre: None, morph: None, gate_length: None, decay: None, .. } 
-                    => print!(" (     )"),
-                SeqStep { note: None, .. } => print!(" ( MOD )"),
-                SeqStep { note: Some(note), model: None, harmonic: None, timbre: None, morph: None, gate_length: None, decay: None, .. } 
-                    => print!(" ({:>+0width$.prec$})", note, width=5, prec=1),
-                SeqStep { note: Some(note), .. } => print!(" MOD({:>+0width$.prec$})", note, width=5, prec=1),
-            };
-            if i % 8 == 7 || (i+1) == self.seq_notes.len() { println!(" ]"); }
-        }
-        println!("");
+    pub fn init (&mut self) {
+        self.synth_engine.init();
     }
 
     pub fn sequencer_loop (arc_synth: Arc<Mutex<Synth>>) {
@@ -305,6 +257,11 @@ impl<'a> Synth<'a> {
         update(value);
         if is_recording { self.record_step(param, *value);}
     }
+
+    
+    pub fn render(&mut self, out: &mut [f32], aux: &mut [f32]) {
+        self.synth_engine.render(&self.patch, &self.modulations, out, aux);
+    }
 }
 
 
@@ -317,22 +274,21 @@ impl<'a> AudioGenerator for Synth<'a> {
         self.patch.lpg_colour = 1.0;
         self.modulations.trigger_patched = true;
         self.modulations.level_patched = true;
-        self.voice.init();
-        self.print_info();
+        self.synth_engine.init();
     }
 
     fn output_sound(&mut self, samples_left: &mut [f32], samples_right: &mut [f32]) {
         let mut out = vec![0.0; self.block_size];
         let mut aux = vec![0.0; self.block_size];
 
-        self.voice.render(&self.patch, &self.modulations, &mut out, &mut aux);
+        self.synth_engine.render(&self.patch, &self.modulations, &mut out, &mut aux);
 
-        // let mut mix = vec![0.0; BLOCK_SIZE];
-        // for frame in 0..BLOCK_SIZE {
-        //     mix[frame] = (out[frame] * (1.0 - self.balance) + aux[frame] * self.balance) * self.volume;
-        // } 
-        samples_left.clone_from_slice(&out);
-        samples_right.clone_from_slice(&out);
+        let mut mix = vec![0.0; self.block_size];
+        for frame in 0..self.block_size {
+            mix[frame] = (out[frame] * (1.0 - self.balance) + aux[frame] * self.balance) * self.volume;
+        } 
+        samples_left.clone_from_slice(&mix);
+        samples_right.clone_from_slice(&mix);
     }
     
     fn process_events(&mut self, event: Event) {
@@ -446,18 +402,6 @@ impl<'a> AudioGenerator for Synth<'a> {
             // Not implemented: self.balance, self.volume, self.patch.lpg_colour
             _ => {}
         }
-        // Print Info
-        match event.event_type {
-            KeyPress(
-                Key::F1 | Key::F2 | Key::F3 | Key::F4 | Key::F5 | Key::F6 | Key::F7 | Key::F8 | Key::F9 | Key::F10 |
-                Key::Space | Key::RightBracket | Key::UpArrow | Key::DownArrow | Key::Dot | Key::Minus | Key::LeftArrow | Key::RightArrow
-            ) => {
-                self.print_info();
-            },
-            KeyPress(key) if notes.contains_key(&key) && self.seq_status == SeqStatus::Recording => self.print_info(),
-            KeyPress(Key::LeftBracket | Key::Backspace) if self.seq_status == SeqStatus::Recording => self.print_info(),
-            _ => ()
-        }
     }
 }
 
@@ -489,7 +433,7 @@ const ENGINE_DESCRIPIONS: [&str; 24] = [
 ];
 
 lazy_static! {
-    static ref notes: HashMap<Key, f32> = HashMap::from(<Vec<(Key, f32)> as TryInto<[(Key, f32); 30]>>::try_into([
+    pub static ref notes: HashMap<Key, f32> = HashMap::from(<Vec<(Key, f32)> as TryInto<[(Key, f32); 30]>>::try_into([
         (Key::Num3,23.), (Key::Num4,26.), (Key::Num5,29.), (Key::Num6,32.), (Key::Num7,35.), (Key::Num8,38.), (Key::Num9,41.),
         (Key::KeyW,22.), (Key::KeyE,25.), (Key::KeyR,28.), (Key::KeyT,31.), (Key::KeyY,34.), (Key::KeyU,37.), (Key::KeyI,40.), (Key::KeyO,43.), 
         (Key::KeyS,01.), (Key::KeyD,04.), (Key::KeyF,07.), (Key::KeyG,10.), (Key::KeyH,13.), (Key::KeyJ,16.), (Key::KeyK,19.),
@@ -514,105 +458,3 @@ fn dec_f64 (value: &mut f64) { *value = (*value - 0.1).max(0.); }
 fn inc_f64 (value: &mut f64) { *value = (*value + 0.1).min(1.); }
 
 
-const VK_Alt: u32 = 164;
-const VK_AltGr: u32 = 165;
-const VK_Backspace: u32 = 0x08;
-const VK_CapsLock: u32 = 20;
-const VK_ControlLeft: u32 = 162;
-const VK_ControlRight: u32 = 163;
-const VK_Delete: u32 = 46;
-const VK_DownArrow: u32 = 40;
-const VK_End: u32 = 35;
-const VK_Escape: u32 = 27;
-const VK_F1: u32 = 112;
-const VK_F10: u32 = 121;
-const VK_F11: u32 = 122;
-const VK_F12: u32 = 123;
-const VK_F2: u32 = 113;
-const VK_F3: u32 = 114;
-const VK_F4: u32 = 115;
-const VK_F5: u32 = 116;
-const VK_F6: u32 = 117;
-const VK_F7: u32 = 118;
-const VK_F8: u32 = 119;
-const VK_F9: u32 = 120;
-const VK_Home: u32 = 36;
-const VK_LeftArrow: u32 = 37;
-const VK_MetaLeft: u32 = 91;
-const VK_PageDown: u32 = 34;
-const VK_PageUp: u32 = 33;
-const VK_Return: u32 = 0x0D;
-const VK_RightArrow: u32 = 39;
-const VK_ShiftLeft: u32 = 160;
-const VK_ShiftRight: u32 = 161;
-const VK_Space: u32 = 32;
-const VK_Tab: u32 = 0x09;
-const VK_UpArrow: u32 = 38;
-const VK_PrintScreen: u32 = 44;
-const VK_ScrollLock: u32 = 145;
-const VK_Pause: u32 = 19;
-const VK_NumLock: u32 = 144;
-const VK_BackQuote: u32 = 192;
-const VK_1: u32 = 49;
-const VK_2: u32 = 50;
-const VK_3: u32 = 51;
-const VK_4: u32 = 52;
-const VK_5: u32 = 53;
-const VK_6: u32 = 54;
-const VK_7: u32 = 55;
-const VK_8: u32 = 56;
-const VK_9: u32 = 57;
-const VK_0: u32 = 48;
-const VK_Minus: u32 = 189;
-const VK_Equal: u32 = 187;
-const VK_Q: u32 = 81;
-const VK_W: u32 = 87;
-const VK_E: u32 = 69;
-const VK_R: u32 = 82;
-const VK_T: u32 = 84;
-const VK_Y: u32 = 89;
-const VK_U: u32 = 85;
-const VK_I: u32 = 73;
-const VK_O: u32 = 79;
-const VK_P: u32 = 80;
-const VK_LeftBracket: u32 = 219;
-const VK_RightBracket: u32 = 221;
-const VK_A: u32 = 65;
-const VK_S: u32 = 83;
-const VK_D: u32 = 68;
-const VK_F: u32 = 70;
-const VK_G: u32 = 71;
-const VK_H: u32 = 72;
-const VK_J: u32 = 74;
-const VK_K: u32 = 75;
-const VK_L: u32 = 76;
-const VK_SemiColon: u32 = 186;
-const VK_Quote: u32 = 222;
-const VK_BackSlash: u32 = 220;
-const VK_IntlBackslash: u32 = 226;
-const VK_Z: u32 = 90;
-const VK_X: u32 = 88;
-const VK_C: u32 = 67;
-const VK_V: u32 = 86;
-const VK_B: u32 = 66;
-const VK_N: u32 = 78;
-const VK_M: u32 = 77;
-const VK_Comma: u32 = 188;
-const VK_Dot: u32 = 190;
-const VK_Slash: u32 = 191;
-const VK_Insert: u32 = 45;
-const VK_KpMinus: u32 = 109;
-const VK_KpPlus: u32 = 107;
-const VK_KpMultiply: u32 = 106;
-const VK_KpDivide: u32 = 111;
-const VK_Kp0: u32 = 96;
-const VK_Kp1: u32 = 97;
-const VK_Kp2: u32 = 98;
-const VK_Kp3: u32 = 99;
-const VK_Kp4: u32 = 100;
-const VK_Kp5: u32 = 101;
-const VK_Kp6: u32 = 102;
-const VK_Kp7: u32 = 103;
-const VK_Kp8: u32 = 104;
-const VK_Kp9: u32 = 105;
-const VK_KpDelete: u32 = 110;
